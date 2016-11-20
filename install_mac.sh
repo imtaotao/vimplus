c5c230cbd9784601c0acfb58c0314b3698755e7a
xuejuntao <xuejuntao@baidu.com>
2016-11-20 08:39:45 +0800

    init

---
+++ install_mac.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#rm -rf ~/.vimrc
+#rm -rf ~/.ycm_extra_conf.py
+
+cp -u .vimrc ~
+cp -u .ycm_extra_conf.py ~
+
+mkdir ~/.vim
+rm -rf ~/.vim/plugin
+rm -rf ~/.vim/colors
+cp -R ./plugin ~/.vim
+cp -R ./colors ~/.vim
+
+mkdir ~/.fonts
+rm -rf ~/.fonts/PowerlineSymbols.otf
+rm -rf ~/.fonts/Droid\ Sans\ Mono\ for\ Powerline\ Nerd\ Font\ Complete.otf
+cp ./fonts/PowerlineSymbols.otf ~/.fonts
+cp ./fonts/Droid\ Sans\ Mono\ for\ Powerline\ Nerd\ Font\ Complete.otf ~/.fonts
+fc-cache -vf ~/.fonts
+
+brew install vim
+brew install go
+brew install clang
+brew install nodejs
+brew install npm
+brew install mono
+
+mkdir -p ~/.config/fontconfig/conf.d
+rm -rf ~/.config/fontconfig/conf.d/10-powerline-symbols.conf
+cp ./fonts/10-powerline-symbols.conf ~/.config/fontconfig/conf.d
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+vim -c "PluginInstall" -c "q" -c "q"
+
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --all
+
+echo "Done!"
+
